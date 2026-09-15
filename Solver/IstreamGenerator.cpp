6e9144b951bc9dadc6555476bf49b894f58897a0
Elizaveta Ryabova <elizaveta.ryabova.04@inbox.ru>
2026-09-15 21:12:34 +0300

    Delete Solver directory

--- Solver/IstreamGenerator.cpp
+++
@@ -1,8 +0,0 @@
-#include "IstreamGenerator.h"
-using namespace std;
-miit::algebra::IstreamGenerator::IstreamGenerator(istream& in) : in(in) {};
-int miit::algebra::IstreamGenerator::generate() {
-	int value = 0;
-	this->in >> value;
-	return value;
-}
