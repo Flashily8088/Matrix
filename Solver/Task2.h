870b6ea7bd6d6ecf765ea2131efcc814268bbac4
Flashily8088 <elizaveta.ryabova.04@inbox.ru>
2026-09-15 20:36:56 +0300

    изменение кодировки

--- Solver/Task2.h
+++ Solver/Task2.h
@@ -4,19 +4,19 @@
 #include "Generator.h"
 #include <iostream>
 using namespace std;
-// Заменить минимальный элемент каждой строки нулём
+// Р—Р°РјРµРЅРёС‚СЊ РјРёРЅРёРјР°Р»СЊРЅС‹Р№ СЌР»РµРјРµРЅС‚ РєР°Р¶РґРѕР№ СЃС‚СЂРѕРєРё РЅСѓР»С‘Рј
 namespace miit::algebra {
 	class Task2 : public Exercise
 	{
 	public:
 		/**
-		* @brief конструкор
-		* @param m - матрица
-		* @param g - генератор
+		* @brief РєРѕРЅСЃС‚СЂСѓРєРѕСЂ
+		* @param m - РјР°С‚СЂРёС†Р°
+		* @param g - РіРµРЅРµСЂР°С‚РѕСЂ
 		*/
 		Task2(Matrix* m, Generator* g);
 		/**
-		* @brief выполняет условия задания 2
+		* @brief РІС‹РїРѕР»РЅСЏРµС‚ СѓСЃР»РѕРІРёСЏ Р·Р°РґР°РЅРёСЏ 2
 		*/
 		void solve() override;
 	};
